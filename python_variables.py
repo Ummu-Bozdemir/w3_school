286ae9eb0a201338b3b80bbe7f2bab06e2d40059
Ummu-Bozdemir <ummubdemir@gmail.com>
2025-04-03 11:15:31 +0200

    Create python_variables.py

---
+++ python_variables.py
@@ -0,0 +1,36 @@
+# Integer 
+temperature = 25
+print("Integer:", temperature)
+
+# Float
+weight = 68.5
+print("Float:", weight)
+
+# String 
+city = "Istanbul"
+print("String:", city)
+
+# List
+fruits = ["apple", "banana", "cherry"]
+print("List:", fruits)
+
+# Tuple 
+coordinates = (40.7128, 74.0060)
+print("Tuple:", coordinates)
+
+# Dictionary 
+car = {"brand": "Tesla", "model": "Model 3"}
+print("Dictionary:", car)
+
+# Boolean 
+is_raining = True
+print("Boolean:", is_raining)
+
+# Set 
+prime_numbers = {2, 3, 5, 7, 11}
+print("Set:", prime_numbers)
+
+# Type chacking
+print("Type of temperature:", type(temperature))
+print("Type of city:", type(city))
+ 
